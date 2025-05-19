82e9115b346b76dd05af19be55e5abe6c715f669
igor.znamensky <igor.znamensky@gmail.com>
2025-05-19 09:26:55 +0300

    feat: Logo asset #001

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import {Button} from "@/components/ui/button";
 export default function Home() {
     return (
         <div>
-            <Button>Button</Button>
-            <p className={'text-emerald-500 font-bold'}>Hello World!</p>
+            <Image src={'./logo.svg'} alt={'Logo'} height={50} width={50}/>
+            <p className={'text-xl font-semibold tracking-tight'}>YouTube Clone</p>
         </div>
     );
 }
