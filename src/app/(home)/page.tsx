502d867f6c618ffa3f76a47a0579daf0d736d52f
igor.znamensky <igor.znamensky@gmail.com>
2025-05-20 09:59:30 +0300

    feat: Home group, home layout, raw side navbar #001

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <div>
             <Image src={'./logo.svg'} alt={'Logo'} height={50} width={50}/>
-            <p className={'text-xl font-semibold tracking-tight'}>YouTube Clone</p>
+            <p className={'text-xl font-semibold tracking-tight py-2'}>YouTube Clone</p>
         </div>
     );
 }
--- src/modules/home/ui/layouts/home-layout.tsx
+++ src/modules/home/ui/layouts/home-layout.tsx
@@ -1,15 +1,19 @@
+import {SidebarProvider} from "@/components/ui/sidebar";
+import HomeNavbar from "@/modules/home/ui/components/home-navbar";
+
 interface HomeLayout {
     children: React.ReactNode
 }
 
 const HomeLayout = ({children}: HomeLayout) => {
     return (
-        <>
-        <p className={'p-4 text-2xl font-bold bg-blue-600'}>
-            Home navBar
-        </p>
-        {children}
-    </>
+        <SidebarProvider>
+            <HomeNavbar />
+            <p className={'w-full'}>
+                Home navBar
+            </p>
+            {children}
+        </SidebarProvider>
     )
 }
 
